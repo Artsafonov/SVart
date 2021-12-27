a6222c45afde78881070cab5b795b7730dc6e851
Artem Safonov <artsafonov19@gmail.com>
2021-12-27 11:43:44 +0100

    обновление

--- src/main/java/com/svart/SVart/SVartApplication.java
+++ src/main/java/com/svart/SVart/SVartApplication.java
@@ -3,11 +3,12 @@ package com.svart.SVart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SVartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SVartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SVartApplication.class, args);
+    }
 
 }
